00781455da45eac91f3e12be935a2cc4c657762f
Lucas pinheiro <lucaslogos@gmail.com>
2023-06-22 14:52:39 -0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Navbar/Navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      NavbarComponent
+   ],
   imports: [
     BrowserModule
   ],
