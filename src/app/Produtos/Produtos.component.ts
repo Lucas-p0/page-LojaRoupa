71a52c58282670bb1b9e86cdbb89e9b9b9a2eca2
Lucas pinheiro <lucaslogos@gmail.com>
2023-06-23 19:10:34 -0300

    update

---
+++ src/app/Produtos/Produtos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Produtos',
+  templateUrl: './Produtos.component.html',
+  styleUrls: ['./Produtos.component.css']
+})
+export class ProdutosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './Navbar/Navbar.component';
 import { BannerComponent } from './Banner/Banner.component';
 import { PromocaoComponent } from './Promocao/Promocao.component';
+import { ProdutosComponent } from './Produtos/Produtos.component';
 
 @NgModule({
-  declarations: [			
+  declarations: [				
     AppComponent,
       NavbarComponent,
       BannerComponent,
-      PromocaoComponent
+      PromocaoComponent,
+      ProdutosComponent
    ],
   imports: [
     BrowserModule
